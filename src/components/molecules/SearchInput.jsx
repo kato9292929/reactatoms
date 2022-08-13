0cc28956cb6f896f219fa454d2032b19956d03dd
kato9292929 <76525458+kato9292929@users.noreply.github.com>
2022-08-13 20:55:40 +0900

    Initial commit

---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton.jsx";
+import { Input } from "../atoms/input/input.jsx";
+
+export const SearchInput = () => {
+  return (
+    <SContainer>
+      <Input placeholder="検索条件を入力" />
+      <SButtonWrapper>
+        <PrimaryButton>検索</PrimaryButton>
+      </SButtonWrapper>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
---
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+export const UserIconWithName = (props) => {
+  const { src, name, isAdmin } = props;
+  return (
+    <SContainer>
+      <SImg
+        height={160}
+        width={160}
+        alt="プロフィール写真"
+        src={image}
+        alt={name}
+      />
+      <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
+
+const SImg = styled.img`
+  border-radius: 50%;
+`;
+
+const SName = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0;
+  color: #40514e;
+`;
+
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
+`;
